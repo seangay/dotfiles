fbceebfcccf46ac49fb75c3e06d87baa288ca6d2
Sean Gay <sean.gay@gmail.com>
2024-08-23 13:55:08 +1000

    feat:added git-credential-manager support

---
+++ git-credential-manager/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+#
+# https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/install.md
+#
+
+echo "› git-credentals-manager"
+brew install --cask git-credential-manager
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#
-# Run all dotfiles installers.
 
 set -e
 
