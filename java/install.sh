9cf5e61c46baaaba60eb0ebb6f6942a38623d415
Sean Gay <sean.gay@gmail.com>
2024-08-27 14:43:13 +1000

    fix: only update Java symlinks if java version is updated

--- java/install.sh
+++ java/install.sh
@@ -6,10 +6,22 @@ set -e
 # https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/install.md
 #
 
+# Get the current Java version
+old_version=$(java -version 2>&1 | head -n 1)
+
 echo "› java"
 brew install java
 
+# Get the "new" Java version after running install
+new_version=$(java -version 2>&1 | head -n 1)
 
-echo "    › setting up symbolic links for system"
-sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+if [ "$old_version" != "$new_version" ]; then
+    echo "Java was installed or upgraded from $old_version to $new_version."
+
+    echo "    › setting up symbolic links for system"
+    sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+else
+    echo "Java was already up-to-date."
+fi
+
 
