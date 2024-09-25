816976a68869eae94fdbb5a038b83ffaf473e0bc
Sean Gay <sean.gay@gmail.com>
2024-09-25 10:33:11 +1000

    add example alias for gradle

---
+++ gradle/aliases.zsh
@@ -0,0 +1,4 @@
+# DO something like this for examining what comprises a bom file as a gradle dep 
+# 
+# ./gradlew :app:dependencyInsight --configuration testRuntimeClasspath --dependency org.junit.jupiter:junit-jupiter
+# 
